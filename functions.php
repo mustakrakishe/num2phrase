92415d3aafe7454d11aef15c877709340926aba7
mustakrakishe <48246354+mustakrakishe@users.noreply.github.com>
2020-10-28 14:49:48 +0200

    Update functions.php
    functions.php. Раскоммит 8.

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
                  if($class != 0){
                      $output .= ' ' . classNum2words($class);
                      $classId = $classCount - $key - 1;
-//                     $output .= getClassEnding($classId, $class);
+                     $output .= getClassEnding($classId, $class);
                  }
             }
         }
@@ -114,101 +114,101 @@
         return implode(' ', $classPhrase);
     }
 
-//     //Возвращает окончание назавние класса (например, для числа 1 234)
-//     //типа ['a', 'тысяч', 'а']
-//     function getClassEnding($classId, $class){
-//         $digitVal = $class % 10;
-//         $twoDigitVals = $class % 100;
-//         if($twoDigitVals < 10 || $twoDigitVals > 20){
-//             $numWordEnding = getUnitEnding([$classId, $digitVal], 0);
-//         }
-//         else{
-//             $numWordEnding = '';
-//         }
-//         $className = getUnitEnding([$classId], 1);
-//         $classNameEnding = getUnitEnding([$classId, $digitVal], 2);
+    //Возвращает окончание назавние класса (например, для числа 1 234)
+    //типа ['a', 'тысяч', 'а']
+    function getClassEnding($classId, $class){
+        $digitVal = $class % 10;
+        $twoDigitVals = $class % 100;
+        if($twoDigitVals < 10 || $twoDigitVals > 20){
+            $numWordEnding = getUnitEnding([$classId, $digitVal], 0);
+        }
+        else{
+            $numWordEnding = '';
+        }
+        $className = getUnitEnding([$classId], 1);
+        $classNameEnding = getUnitEnding([$classId, $digitVal], 2);
         
-//         return $numWordEnding . ' ' . $className . $classNameEnding;
-//     }
+        return $numWordEnding . ' ' . $className . $classNameEnding;
+    }
 
-//     //Возвращает окончание для элемента указанного типа в $unitType:
-//     //0 - окончание чисельника;
-//     //1 - название класса чисел;
-//     //2 - окончание названия класса чисел.
-//     //$values - указанные параметры типа [$classId, $lastNumDigit], или [$classId],
-//     //в зависимости от $unitType и соответствующего правила.
-//     function getUnitEnding($values, $unitType){
-//         //Правила определения названия класса вида [classId, digitVal, numWordEnding].
-//         //'' для classId и digitVal означает любое значение, не включеное в предыдущие правила.
-//         //[min, max] для classId и digitVal означает диамазон.
-//         $numWordEndingRules = [
-//             [1, 1, 'на'],
-//             ['', 1, 'ин'],
-//             [1, 2, 'е'],
-//             ['', 2, 'а'],
-//             ['', '', '']
-//         ];
+    //Возвращает окончание для элемента указанного типа в $unitType:
+    //0 - окончание чисельника;
+    //1 - название класса чисел;
+    //2 - окончание названия класса чисел.
+    //$values - указанные параметры типа [$classId, $lastNumDigit], или [$classId],
+    //в зависимости от $unitType и соответствующего правила.
+    function getUnitEnding($values, $unitType){
+        //Правила определения названия класса вида [classId, digitVal, numWordEnding].
+        //'' для classId и digitVal означает любое значение, не включеное в предыдущие правила.
+        //[min, max] для classId и digitVal означает диамазон.
+        $numWordEndingRules = [
+            [1, 1, 'на'],
+            ['', 1, 'ин'],
+            [1, 2, 'е'],
+            ['', 2, 'а'],
+            ['', '', '']
+        ];
 
-//         //Правила определения названия класса вида [classId, className].
-//         //'' для classId означает любое значение, не включеное в предыдущие правила.
-//         //[min, max] для classId означает диамазон.
-//         $classNameRules = [
-//             [0, ''],
-//             [1, 'тысяч'],
-//             [2, 'миллион'],
-//             [3, 'миллиард'],
-//             ['', 'чего-то']
-//         ];
+        //Правила определения названия класса вида [classId, className].
+        //'' для classId означает любое значение, не включеное в предыдущие правила.
+        //[min, max] для classId означает диамазон.
+        $classNameRules = [
+            [0, ''],
+            [1, 'тысяч'],
+            [2, 'миллион'],
+            [3, 'миллиард'],
+            ['', 'чего-то']
+        ];
 
-//         //Правила определения названия класса вида [classId, digitVal, ending].
-//         //'' для classId и digitVal означает любое значение, не включеное в предыдущие правила.
-//         //[min, max] для classId и digitVal означает диамазон.
-//         $classNameEndingRuls = [
-//             [0, '', ''],
-//             [1, 1, 'а'],
-//             [1, [2, 4], 'и'],
-//             [1, '', ''],
-//             [2, 1, ''],
-//             [2, [2, 4], 'a'],
-//             [2, '', 'ов'],
-//             [3, 1, ''],
-//             [3, [2, 4], 'a'],
-//             [3, '', 'ов'],
-//             ['', '', ''],
-//         ];
+        //Правила определения названия класса вида [classId, digitVal, ending].
+        //'' для classId и digitVal означает любое значение, не включеное в предыдущие правила.
+        //[min, max] для classId и digitVal означает диамазон.
+        $classNameEndingRuls = [
+            [0, '', ''],
+            [1, 1, 'а'],
+            [1, [2, 4], 'и'],
+            [1, '', ''],
+            [2, 1, ''],
+            [2, [2, 4], 'a'],
+            [2, '', 'ов'],
+            [3, 1, ''],
+            [3, [2, 4], 'a'],
+            [3, '', 'ов'],
+            ['', '', ''],
+        ];
 
-//         $rules = [$numWordEndingRules, $classNameRules, $classNameEndingRuls][$unitType];
-//         $maxValueId = count($values) - 1;
+        $rules = [$numWordEndingRules, $classNameRules, $classNameEndingRuls][$unitType];
+        $maxValueId = count($values) - 1;
 
-//         foreach($rules as $rule){
+        foreach($rules as $rule){
 
-//             foreach($values as $valueId => $value){
-//                 $ruleVal = $rule[$valueId];
+            foreach($values as $valueId => $value){
+                $ruleVal = $rule[$valueId];
                 
-//                 if(!compPar($ruleVal, $value)){
-//                     break;
-//                 }
-//                 elseif($valueId == $maxValueId){
-//                     $unitEnding = $rule[$maxValueId + 1];
-//                 }
-//             }
+                if(!compPar($ruleVal, $value)){
+                    break;
+                }
+                elseif($valueId == $maxValueId){
+                    $unitEnding = $rule[$maxValueId + 1];
+                }
+            }
 
-//             if(isset($unitEnding)){
-//                 break;
-//             }
-//         }
+            if(isset($unitEnding)){
+                break;
+            }
+        }
 
-//         return $unitEnding;
-//     }
+        return $unitEnding;
+    }
     
-//     //проверяет, удовлетворяет ли фактическое значение $factVal зачению из правила $ruleVal
-//     function compPar($ruleVal, $factVal){
-//         if(gettype($ruleVal) != 'array'){
-//             $isParMatch = ($ruleVal == $factVal || $ruleVal === '');
-//         }
-//         else{
-//             $isParMatch = ($factVal >= $ruleVal[0] && $factVal <= $ruleVal[1]);
-//         }
-//         return $isParMatch;
-//     }
+    //проверяет, удовлетворяет ли фактическое значение $factVal зачению из правила $ruleVal
+    function compPar($ruleVal, $factVal){
+        if(gettype($ruleVal) != 'array'){
+            $isParMatch = ($ruleVal == $factVal || $ruleVal === '');
+        }
+        else{
+            $isParMatch = ($factVal >= $ruleVal[0] && $factVal <= $ruleVal[1]);
+        }
+        return $isParMatch;
+    }
 ?>
