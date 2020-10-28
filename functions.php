3b30b72fa038d8c660f28fa8b55ab2a01eba524f
mustakrakishe <48246354+mustakrakishe@users.noreply.github.com>
2020-10-28 16:31:52 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@
                  }
             }
         }
-        $output = trim($output);
-        return $firstChar = mb_substr($output, 0, 1);
+        return $output = trim($output);
+//         $firstChar = mb_substr($output, 0, 1);
 //         return $output = mb_strtoupper($firstChar) . mb_substr($output, 1);
     }
 
